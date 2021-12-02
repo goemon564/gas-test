eff389ba951b383b5249725f1862340df52194c2
goemon564 <yz4gchea@gmail.com>
2021-12-02 23:02:45 +0900

    feat: 環境別にソースコードをpushできるように実装

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 function myFunc() {
     console.log("Hello, World.")
     console.log("shell")
+    console.log("test")
+    console.log("hogeeeeee")
 }
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,19 @@
 
 echo "shellを起動"
 
-command="clasp push"
-eval $command
+devScriptId='{"scriptId":"1haaORBkPw_C6_V_H289SsHKKX5dYI94cXUcVU5g8Gj37GSDzs2tWUTPW"}'
+prdScriptId='{"scriptId":"1QspfVSogtAphpD9kxbOyx_7ov14OtafwbQVEEo2HbihXSoIK1TTApu1v"}'
+
+if [ $1 = "-e" -a $2 = "dev" ]; then
+    echo "dev環境に反映します"
+    eval echo '${devScriptId}' > .clasp.json
+    eval "clasp push"
+    # eval echo "" > .clasp.json
+elif [ $1 = "-e" -a $2 = "prd" ]; then
+    echo "prd環境に反映します"
+    eval echo '${prdScriptId}' > .clasp.json
+    eval "clasp push"
+    # eval echo "" > .clasp.json
+else
+    echo "コマンドが足りません"
+fi
