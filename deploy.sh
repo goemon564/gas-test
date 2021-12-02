fb4fa208ebef3c953d12036fc3b659f2b3a754f6
goemon564 <yz4gchea@gmail.com>
2021-12-02 23:22:16 +0900

    fix: 今いるブランチ名を取得

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,11 @@ echo "shellを起動"
 
 devScriptId='{"scriptId":"1haaORBkPw_C6_V_H289SsHKKX5dYI94cXUcVU5g8Gj37GSDzs2tWUTPW"}'
 prdScriptId='{"scriptId":"1QspfVSogtAphpD9kxbOyx_7ov14OtafwbQVEEo2HbihXSoIK1TTApu1v"}'
+currentBranch= eval 'git branch --contains | cut -d " " -f 2'
 
 if [ $1 = "-e" -a $2 = "dev" ]; then
     echo "dev環境に反映します"
+    echo "${currentBranch}"
     eval echo '${devScriptId}' > .clasp.json
     eval "git checkout develop"
     eval "git pull"
